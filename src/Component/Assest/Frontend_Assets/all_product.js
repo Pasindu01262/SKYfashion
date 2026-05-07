6815f025f3b0fcb232db9921079733f7b6b6542e
Pasindu Ransika Kossinna <196893365+Pasindu01262@users.noreply.github.com>
2026-05-08 03:54:04 +0845

    Update all_product.js

--- src/Component/Assest/Frontend_Assets/all_product.js
+++ src/Component/Assest/Frontend_Assets/all_product.js
@@ -3,7 +3,7 @@ import one from './1.png'
 import two from './2.png'
 import three from './3.jpeg'
 import four from './4.jpeg'
-import five from './5.jpeg'
+
 
 
 let data_product = [
@@ -40,13 +40,7 @@ let data_product = [
     color:"White",
   },
 
-   {id:50,
-    name:"Premium Striped Textured Shirt",
-    image:five,
-    new_price:1599,
-    old_price:1800,
-    color:"White",
-  },
+ 
 
   
 
