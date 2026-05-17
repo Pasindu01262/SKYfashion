9f11660446fe4d31097214d578cfd68e43278f01
Pasindu Ransika Kossinna <196893365+Pasindu01262@users.noreply.github.com>
2026-05-17 21:39:41 +0845

    Update new_collections.js

--- src/Component/Assest/Frontend_Assets/new_collections.js
+++ src/Component/Assest/Frontend_Assets/new_collections.js
@@ -1,52 +1,111 @@
 
 import one from './1.png'
-import two from './2.png'
+import two from './6.jpeg'
 import three from './3.jpeg'
-import four from './4.jpeg'
+import four from './7.jpeg'
 import five from './5.jpeg'
+import six from './8.jpeg'
+import seventh from './9.jpeg'
+import eight from './10.jpeg'
+import nine from './11.jpeg'
+import ten from './12.jpeg'
+import leven from './13.jpeg'
 
 
 let new_collections = [
-    {id:10,
-     name:"Zip-neck T-shirt",
-     image:one,
-     new_price:1599,
-     old_price:1800,
-     color:"Green and White",
-   },
+  {id:10,
+    name:"Zip-neck T-shirt",
+    image:one,
+    new_price:1599,
+    old_price:1800,
+    color:"Green and White",
+  },
+ 
+  {id:20,
+    name:"Cotton Single T-shirt",
+    image:two,
+    new_price:1499,
+    old_price:1750,
+    color:"White",
+  },
+
   
-   {id:20,
-     name:"Zip-neck T-shirt",
-     image:two,
-     new_price:1599,
-     old_price:1800,
-     color:"Blue and White",
-   },
- 
-   
-   {id:30,
-     name:"Premium Striped Textured Shirt",
-     image:three,
-     new_price:1599,
-     old_price:1800,
-     color:"White",
-   },
- 
-    {id:40,
-     name:"Premium Striped Textured Shirt",
-     image:four,
-     new_price:1599,
-     old_price:1800,
-     color:"White",
-   },
- 
+  {id:30,
+    name:"Premium Striped Textured Shirt",
+    image:three,
+    new_price:1599,
+    old_price:1800,
+    color:"White",
+  },
+
+   {id:40,
+    name:"Cotton Single T-shirt",
+    image:four,
+    new_price:1499,
+    old_price:1750,
+    color:"Dark Blue",
+  },
+
+
     {id:50,
-     name:"Premium Striped Textured Shirt",
-     image:five,
-     new_price:1599,
-     old_price:1800,
-     color:"White",
-   },
+    name:"Premium Striped Textured Shirt",
+    image:five,
+    new_price:1599,
+    old_price:1800,
+    color:"White",
+  },
+
+
+   {id:60,
+    name:"Cotton Single T-shirt",
+    image:six,
+    new_price:1499,
+    old_price:1750,
+    color:"Maroon",
+  },
+
+    {id:70,
+    name:"Cotton Single T-shirt",
+    image:seventh,
+    new_price:1499,
+    old_price:1750,
+    color:"Maroon",
+  },
+
+
+     {id:80,
+    name:"Premium Striped Textured Shirt",
+    image:eight,
+    new_price:1999,
+    old_price:2500,
+    color:"White",
+  },
+
+
+      {id:90,
+    name:"Premium Striped Textured Shirt",
+    image:nine,
+    new_price:1999,
+    old_price:2500,
+    color:"White",
+  },
+
+   {id:100,
+    name:"Premium Linen Blend(Cotton Linen shirt)",
+    image:ten,
+    new_price:2199,
+    old_price:3000,
+    color:"White",
+  },
+
+     {id:110,
+    name:"Premium Striped Textured Shirt",
+    image:leven,
+    new_price:1999,
+    old_price:2500,
+    color:"White",
+  },
+
  
 
 ];
