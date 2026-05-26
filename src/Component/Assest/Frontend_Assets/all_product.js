cb9b62e83732af7afbb4136ee657e8763106dbab
Pasindu Ransika Kossinna <196893365+Pasindu01262@users.noreply.github.com>
2026-05-26 14:23:11 +0845

    Update all_product.js

--- src/Component/Assest/Frontend_Assets/all_product.js
+++ src/Component/Assest/Frontend_Assets/all_product.js
@@ -1,14 +1,9 @@
 
 import one from './1.png'
-import two from './6.jpeg'
 import three from './3.jpeg'
-import four from './7.jpeg'
 import five from './5.jpeg'
-import six from './8.jpeg'
-import seventh from './9.jpeg'
 import eight from './10.jpeg'
 import nine from './11.jpeg'
-import ten from './12.jpeg'
 import leven from './13.jpeg'
 
 
@@ -20,15 +15,6 @@ let data_product = [
     old_price:1800,
     color:"Green and White",
   },
- 
-  {id:20,
-    name:"Cotton Single T-shirt",
-    image:two,
-    new_price:1499,
-    old_price:1750,
-    color:"White",
-  },
-
   
   {id:30,
     name:"Premium Striped Textured Shirt",
@@ -36,14 +22,6 @@ let data_product = [
     new_price:1599,
     old_price:1800,
     color:"White",
-  },
-
-   {id:40,
-    name:"Cotton Single T-shirt",
-    image:four,
-    new_price:1499,
-    old_price:1750,
-    color:"Dark Blue",
   },
 
 
@@ -55,22 +33,6 @@ let data_product = [
     color:"White",
   },
 
-
-   {id:60,
-    name:"Cotton Single T-shirt",
-    image:six,
-    new_price:1499,
-    old_price:1750,
-    color:"Maroon",
-  },
-
-    {id:70,
-    name:"Cotton Single T-shirt",
-    image:seventh,
-    new_price:1499,
-    old_price:1750,
-    color:"Maroon",
-  },
 
 
      {id:80,
@@ -90,13 +52,7 @@ let data_product = [
     color:"White",
   },
 
-   {id:100,
-    name:"Premium Linen Blend(Cotton Linen shirt)",
-    image:ten,
-    new_price:2199,
-    old_price:3000,
-    color:"White",
-  },
+
 
      {id:110,
     name:"Premium Striped Textured Shirt",
