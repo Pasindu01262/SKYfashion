321655a30be10aff19fb75da426f956367808200
Pasindu Ransika Kossinna <196893365+Pasindu01262@users.noreply.github.com>
2026-05-26 14:43:00 +0845

    Update all_product.js

--- src/Component/Assest/Frontend_Assets/all_product.js
+++ src/Component/Assest/Frontend_Assets/all_product.js
@@ -1,10 +1,14 @@
 
 import one from './1.png'
-import three from './3.jpeg'
-import five from './5.jpeg'
-import eight from './10.jpeg'
-import nine from './11.jpeg'
-import leven from './13.jpeg'
+import two from './3.jpeg'
+import three from './5.jpeg'
+import four from './10.jpeg'
+import five from './11.jpeg'
+import six from './13.jpeg'
+import seven from './14.jpeg'
+import eight from './15.jpeg'
+import nine from './17.jpeg'
+import ten from './18.jpeg'
 
 
 let data_product = [
@@ -16,7 +20,16 @@ let data_product = [
     color:"Green and White",
   },
   
-  {id:30,
+  {id:20,
+    name:"Premium Striped Textured Shirt",
+    image:two,
+    new_price:1599,
+    old_price:1800,
+    color:"White",
+  },
+
+
+    {id:30,
     name:"Premium Striped Textured Shirt",
     image:three,
     new_price:1599,
@@ -25,39 +38,63 @@ let data_product = [
   },
 
 
-    {id:50,
+
+     {id:40,
+    name:"Premium Striped Textured Shirt",
+    image:four,
+    new_price:1999,
+    old_price:2500,
+    color:"White",
+  },
+
+
+      {id:50,
     name:"Premium Striped Textured Shirt",
     image:five,
-    new_price:1599,
-    old_price:1800,
-    color:"White",
-  },
-
-
-
-     {id:80,
-    name:"Premium Striped Textured Shirt",
-    image:eight,
     new_price:1999,
     old_price:2500,
     color:"White",
   },
 
 
-      {id:90,
+
+     {id:60,
+    name:"Premium Striped Textured Shirt",
+    image:six,
+    new_price:1999,
+    old_price:2500,
+    color:"White",
+  },
+
+       {id:70,
+    name:"Premium Linen Blend(Cotton Linen shirt)",
+    image:seven,
+    new_price:2299,
+    old_price:2600,
+    color:"White",
+  },
+
+        {id:80,
+    name:"Premium Linen Blend(Cotton Linen shirt)",
+    image:eight,
+    new_price:2299,
+    old_price:2600,
+    color:"White",
+  },
+
+          {id:90,
     name:"Premium Striped Textured Shirt",
     image:nine,
-    new_price:1999,
+    new_price:2199,
     old_price:2500,
     color:"White",
   },
 
 
-
-     {id:110,
+             {id:100,
     name:"Premium Striped Textured Shirt",
-    image:leven,
-    new_price:1999,
+    image:ten,
+    new_price:2199,
     old_price:2500,
     color:"White",
   },
