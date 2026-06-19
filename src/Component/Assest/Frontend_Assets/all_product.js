d682c174ef2e17957825988ab7fe70edda1a43da
pasindu <pasindukossinna@gmail.com>
2026-06-19 23:44:00 +0845

    Update website with minor changes

--- src/Component/Assest/Frontend_Assets/all_product.js
+++ src/Component/Assest/Frontend_Assets/all_product.js
@@ -1,7 +1,7 @@
 
 import one from './1.png'
-import two from './2.png'
-import three from './3.jpeg'
+import two from './2.jpeg'
+import three from './3.png'
 import four from './4.jpeg'
 import five from './5.jpeg'
 
@@ -10,41 +10,41 @@ let data_product = [
    {id:10,
     name:"Zip-neck T-shirt",
     image:one,
-    new_price:1599,
-    old_price:1800,
+    new_price:1699,
+    old_price:1850,
     color:"Green and White",
   },
  
   {id:20,
-    name:"Zip-neck T-shirt",
+    name:"Premium Striped Textured Shirt",
     image:two,
-    new_price:1599,
-    old_price:1800,
-    color:"Blue and White",
+    new_price:1699,
+    old_price:1850,
+    color:"White",
   },
 
   
   {id:30,
     name:"Premium Striped Textured Shirt",
     image:three,
-    new_price:1599,
-    old_price:1800,
+    new_price:1999,
+    old_price:2300,
     color:"White",
   },
 
    {id:40,
-    name:"Premium Striped Textured Shirt",
+    name:"Premium Linen Blend(Cotton Linen shirt)",
     image:four,
-    new_price:1599,
-    old_price:1800,
+    new_price:2390,
+    old_price:2800,
     color:"White",
   },
 
    {id:50,
-    name:"Premium Striped Textured Shirt",
+    name:"Premium remium Linen Blend(Cotton Linen shirt)",
     image:five,
-    new_price:1599,
-    old_price:1800,
+    new_price:2390,
+    old_price:2800,
     color:"White",
   },
 
--- src/Component/Assest/Frontend_Assets/new_collections.js
+++ src/Component/Assest/Frontend_Assets/new_collections.js
@@ -1,53 +1,52 @@
 
 import one from './1.png'
-import two from './2.png'
-import three from './3.jpeg'
+import two from './2.jpeg'
+import three from './3.png'
 import four from './4.jpeg'
 import five from './5.jpeg'
 
 
 let new_collections = [
-    {id:10,
-     name:"Zip-neck T-shirt",
-     image:one,
-     new_price:1599,
-     old_price:1800,
-     color:"Green and White",
-   },
+        {id:10,
+    name:"Zip-neck T-shirt",
+    image:one,
+    new_price:1699,
+    old_price:1850,
+    color:"Green and White",
+  },
+ 
+  {id:20,
+    name:"Premium Striped Textured Shirt",
+    image:two,
+    new_price:1699,
+    old_price:1850,
+    color:"White",
+  },
+
   
-   {id:20,
-     name:"Zip-neck T-shirt",
-     image:two,
-     new_price:1599,
-     old_price:1800,
-     color:"Blue and White",
-   },
- 
-   
-   {id:30,
-     name:"Premium Striped Textured Shirt",
-     image:three,
-     new_price:1599,
-     old_price:1800,
-     color:"White",
-   },
- 
-    {id:40,
-     name:"Premium Striped Textured Shirt",
-     image:four,
-     new_price:1599,
-     old_price:1800,
-     color:"White",
-   },
- 
-    {id:50,
-     name:"Premium Striped Textured Shirt",
-     image:five,
-     new_price:1599,
-     old_price:1800,
-     color:"White",
-   },
- 
+  {id:30,
+    name:"Premium Striped Textured Shirt",
+    image:three,
+    new_price:1999,
+    old_price:2300,
+    color:"White",
+  },
+
+   {id:40,
+    name:"Premium Linen Blend(Cotton Linen shirt)",
+    image:four,
+    new_price:2390,
+    old_price:2800,
+    color:"White",
+  },
+
+   {id:50,
+    name:"Premium remium Linen Blend(Cotton Linen shirt)",
+    image:five,
+    new_price:2390,
+    old_price:2800,
+    color:"White",
+  },
 
 ];
 
